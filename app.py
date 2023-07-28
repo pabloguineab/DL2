661382da9361223212cc1c87a7b14f3fdcf44414
Pablo Guinea <48864366+pabloguineab@users.noreply.github.com>
2023-07-28 13:22:14 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,37 +1,64 @@
+# Import required libraries
 import streamlit as st
 import cv2
 import numpy as np
 import tensorflow as tf
+from PIL import Image
 from utils import label_map_util
 from utils import visualization_utils as vis_util
 
-@st.cache(allow_output_mutation=True)
-def load_model():
-    # Your model loading code here...
+# Constants
+MODEL_NAME = 'models/inference_graph'
+PATH_TO_CKPT = MODEL_NAME + '/frozen_inference_graph.pb'
+PATH_TO_LABELS = 'training/labelmap.pbtxt'
+NUM_CLASSES = 6
 
-@st.cache(allow_output_mutation=True)
-def predict_video(sess, detection_graph, category_index, video_file):
-    # Open the video file
-    vidcap = cv2.VideoCapture(video_file)
+# Load the label map
+label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
+categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
+category_index = label_map_util.create_category_index(categories)
 
-    # Loop over each frame
-    while True:
-        success, image_np = vidcap.read()
-        if not success:
-            break
+# Load the Tensorflow model into memory
+detection_graph = tf.Graph()
+with detection_graph.as_default():
+    od_graph_def = tf.GraphDef()
+    with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+        serialized_graph = fid.read()
+        od_graph_def.ParseFromString(serialized_graph)
+        tf.import_graph_def(od_graph_def, name='')
+sess = tf.Session(graph=detection_graph)
 
-        # Your prediction code here...
+# Define input and output tensors
+image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
+detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
+num_detections = detection_graph.get_tensor_by_name('num_detections:0')
 
-    # Close the video file
-    vidcap.release()
+def run_inference(image):
+    # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+    image_np_expanded = np.expand_dims(image, axis=0)
+    # Actual detection
+    (boxes, scores, classes, num) = sess.run(
+        [detection_boxes, detection_scores, detection_classes, num_detections],
+        feed_dict={image_tensor: image_np_expanded})
+    return boxes, scores, classes, num
 
-st.title("Poker Card Identifier")
-
-uploaded_file = st.file_uploader("Choose a video...", type="mp4")
-
+st.title("Object Detection App")
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "png"])
 if uploaded_file is not None:
-    st.video(uploaded_file)
-    st.write("Predicting...")
-
-    # Call the prediction function
-    predict_video(sess, graph, category_index, uploaded_file)
+    image = Image.open(uploaded_file)
+    image_np = np.array(image)
+    st.image(image, caption='Uploaded Image.', use_column_width=True)
+    st.write("")
+    st.write("Detecting...")
+    boxes, scores, classes, num = run_inference(image_np)
+    vis_util.visualize_boxes_and_labels_on_image_array(
+        image_np,
+        np.squeeze(boxes),
+        np.squeeze(classes).astype(np.int32),
+        np.squeeze(scores),
+        category_index,
+        use_normalized_coordinates=True,
+        line_thickness=8)
+    st.image(image_np, caption='Detected Objects.', use_column_width=True)
